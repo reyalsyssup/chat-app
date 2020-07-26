76568e51e00a9de246cd1f974baff165eaf5631b
reyalsyssup <63643459+reyalsyssup@users.noreply.github.com>
2020-07-26 18:42:44 +1000

    inital commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express"),
+app = express(),
+http = require("http").createServer(app),
+io = require("socket.io")(http),
+fs = require("fs");
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+let name = "";
+
+io.on("connection", (socket) => {
+    console.log("User connected!");
+    socket.on("disconnect", () => {
+        console.log("User disconnected!");
+    });
+
+    socket.on("send msg", (msg) => {
+        // console.log(msg);
+        io.emit("send msg", msg);
+        fs.appendFileSync(__dirname + "/log.txt", msg + "\n");
+    });
+
+});
+
+app.get("/", (req, res) => {
+    res.render("chat");
+});
+
+http.listen(3000, () => console.log("Server listening on port 3000"));
---
+++ public/main.js
@@ -0,0 +1,58 @@
+const socket = io();
+const form = document.querySelector("form");
+const input = document.querySelector("#msg-input");
+const chatlog = document.querySelector("#log");
+const changeNameBtn = document.querySelector("#change-name-btn");
+const changeNameInput = document.querySelector("#change-name-input");
+
+let name = "";
+
+try {
+    name = localStorage.getItem("name");
+    changeNameInput.value = name;
+} catch{}
+
+function addMsg(msg, type) {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(`${msg}`));
+    chatlog.appendChild(li);
+    if(type === "error") li.style.backgroundColor = "#ff4f4f";
+    else if(type === "warning") li.style.backgroundColor = "#ffbb3d";
+    window.scrollTo(0,document.body.scrollHeight);
+}
+
+// checking if you press enter in the change name area
+document.addEventListener("keydown", (event) => {
+    // 69 is keycode 'e'
+    if(event.keyCode === 13) {
+        // user is focused in the change name box
+        if(document.activeElement === changeNameInput) {
+            if(changeNameInput.value !== "") {
+                name = changeNameInput.value;
+                localStorage.setItem("name", name);
+            }
+            else addMsg("Name must not be empty!", "error");
+        }
+    }
+});
+
+changeNameBtn.addEventListener("click", () => {
+    if(changeNameInput.value !== "") name = changeNameInput.value;
+    else addMsg("Name must not be empty!", "error");
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if(name !== null) {
+        let msg = input.value;
+        // if the string is only whitespace
+        if(msg.replace(/\s/g, '').length !== 0) {
+            socket.emit("send msg", `${name}: ${msg}`);
+            input.value = "";
+        } else addMsg("Message cannot be empty!", "warning");
+    } else addMsg("Please enter a name in the top right!", "error");
+});
+
+socket.on("send msg", (msg) => {
+    addMsg(msg);
+});
